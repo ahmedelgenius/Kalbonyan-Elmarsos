a7a5aaac57bc390cf5b67de756997842534f8bfe
Ahmed Hany <90596200+ahmedelgenius@users.noreply.github.com>
2022-07-08 13:05:55 +0200

    Add files via upload

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
@@ -1,1 +1,12 @@
+def plant_recommendation(care):
+    if care == 'low':
+        print('alone')
+    elif care == 'medium':
+        print('pothos')
+    elif care == 'high':
+        print('orchid')
 
+
+plant_recommendation('low')
+plant_recommendation('medium')
+plant_recommendation('high')
