a91f44cad63f77aae6f28d92496c193ff0b144b8
Ahmed Hany <90596200+ahmedelgenius@users.noreply.github.com>
2022-07-08 13:04:02 +0200

    Add files via upload

---
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/03.py
@@ -0,0 +1,14 @@
+
+# Greet the user 
+
+name = input("what's your name? ") # asks the user their name
+print ("It's nice to meet you," ,name)
+
+
+answer = input ("Are you enjoying the course?")
+
+
+if answer == "yes":
+    print ("That's good to hear!")
+else :
+    print ("Oh no! That makes me sad!")
--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
@@ -1,1 +1,8 @@
 
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+
+favorite_city("Sata Barbara, California")
+favorite_city("Asheville, North Carolina")
+favorite_city("Amsterdam , The Netherlands")
