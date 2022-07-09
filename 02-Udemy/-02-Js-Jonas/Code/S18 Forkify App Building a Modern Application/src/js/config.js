8bea8e4b8e167d351efeb3adfa8e6f7551eebdac
Ahmed Hany <90596200+ahmedelgenius@users.noreply.github.com>
2022-07-09 22:15:58 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Code/S18 Forkify App Building a Modern Application/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "b143fdd6-8f29-4b83-9d18-f836f8986464";
+export const MODAL_CLOSE_SEC = 2.5;
