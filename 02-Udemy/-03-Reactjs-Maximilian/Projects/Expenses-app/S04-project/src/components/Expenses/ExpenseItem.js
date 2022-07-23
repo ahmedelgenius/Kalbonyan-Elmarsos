ee8bde632f70641a6da81dda5300e542ca19fcbd
Ahmed Hany <90596200+ahmedelgenius@users.noreply.github.com>
2022-07-23 13:03:32 +0200

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/Expenses-app/S04-project/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,30 @@
+// import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  // const [title, setTitle] = useState(props.title);
+  // console.log("expenseItem evaluated by react");
+  // const title = props.title;
+  // const clickHandler = () => {
+  //   setTitle("updated");
+  // };
+  //   return <h2> expense item </h2>;
+  //   const expenseDate = new Date(2022, 7, 15);
+  //   const expenseTitle = "car insurance";
+  //   const expenseAmount = 290.29;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
