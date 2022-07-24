47cd31074143086be6a0164eb41934d2733684e3
Ahmed Hany <90596200+ahmedelgenius@users.noreply.github.com>
2022-07-24 03:01:33 +0200

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Code/S29/src/App.js
@@ -0,0 +1,34 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  // return <div>hello</div>
+  //   return <div>
+  //     <h1>my todos</h1>
+  //   </div>
+  //   return <div>
+  //     <h2>title</h2>
+  // <button> delete</button>
+  //   </div>
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
